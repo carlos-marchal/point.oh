4e46a30c4cd3c06b3d55f0d43e0ecf374ffe4f4a
Carlos Marchal <marchal.carlos@pm.me>
2022-07-23 10:55:13 +0200

    changed vite build configuration to build playground

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
-import path from "path";
 import { UserConfig } from "vite";
 
 const config: UserConfig = {
   build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/mod.ts"),
-      name: "point.oh",
-    },
     outDir: "build",
-    sourcemap: true,
   },
 };
 
